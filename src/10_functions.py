387f782aba213cd6ee5bffc33a7575cd18aacd81
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-25 16:36:18 -0400

    Adding functions.

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,27 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
-# YOUR CODE HERE
+def get_even(x):
+ if x % 2 == 0:
+  print(True)
+ else:
+  print(False)
 
+print(
+ get_even(4),
+ get_even(3)
+)
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+# Num is here three times with no name collisions, I think that's really cool.
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def even_odd(num):
+ if num % 2 == 0:
+  print("Even")
+ else:
+  print("Odd")
+
+print(even_odd(num))
 
